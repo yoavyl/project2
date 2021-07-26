e031f576e2f24f3b3388fdf7daa201ead6581a15
yoavyl <yoav.lip@gmail.com>
2021-07-26 19:40:13 +0300

    draws static chart

--- main6.js
+++ main6.js
@@ -50,6 +50,7 @@ $(function () {
         $("#containerDiv").css({"left": "20%", "margin-top" : "5px"}).show();
         $(".data").show();
     });
+    
     $("#live").click( function () {
         $("#live").css({"background-color" : "#2196F3", "color": "white"});
         $("#home, #about").css({"background-color" : "white", "color": "#2196F3"});
@@ -264,99 +265,111 @@ $(function () {
             console.log("coin: " + coin);
             console.log("USD :" + topFiveUSD[coin].USD);
         }    
-    }          
+    } 
+    
+    $("#live").click(function () {
+        drawChart();
+    })
+    
+    function drawChart() {
+        var options = {
+            width: 800,
+            exportEnabled: true,
+            animationEnabled: true,
+            title:{
+                text: "Units Sold VS Profit"
+            },
+            subtitles: [{
+                text: "Click Legend to Hide or Unhide Data Series"
+            }],
+            axisX: {
+                title: "States"
+            },
+            axisY: {
+                title: "Coin 1",
+                titleFontColor: "#4F81BC",
+                lineColor: "#4F81BC",
+                labelFontColor: "#4F81BC",
+                tickColor: "#4F81BC"
+            },
+            axisY2: {
+                title: "Coin 2",
+                titleFontColor: "#C0504E",
+                lineColor: "#C0504E",
+                labelFontColor: "#C0504E",
+                tickColor: "#C0504E"
+            },
+            axisY3: {
+                title: "Coin 3",
+                titleFontColor: "Yellow",
+                lineColor: "Yellow",
+                labelFontColor: "Yellow",
+                tickColor: "Yellow"
+            },
+            toolTip: {
+                shared: true
+            },
+            legend: {
+                cursor: "pointer",
+                itemclick: toggleDataSeries
+            },
+            data: [{
+                type: "spline",
+                name: "Coin 1",
+                showInLegend: true,
+                xValueFormatString: "MMM YYYY",
+                yValueFormatString: "#,##0 Units",
+                dataPoints: [
+                    { x: new Date(2016, 0, 1),  y: 120 },
+                    { x: new Date(2016, 1, 1), y: 135 },
+                    { x: new Date(2016, 2, 1), y: 144 },
+                    { x: new Date(2016, 3, 1),  y: 103 },
+                    { x: new Date(2016, 4, 1),  y: 93 },
+                    { x: new Date(2016, 5, 1),  y: 129 },
+                    { x: new Date(2016, 6, 1), y: 143 },
+                    { x: new Date(2016, 7, 1), y: 156 },
+                    { x: new Date(2016, 8, 1),  y: 122 },
+                    { x: new Date(2016, 9, 1),  y: 106 },
+                    { x: new Date(2016, 10, 1),  y: 137 },
+                    { x: new Date(2016, 11, 1), y: 142 }
+                ]
+            },
+            {
+                type: "spline",
+                name: "Coin2",
+                axisYType: "secondary",
+                showInLegend: true,
+                xValueFormatString: "MMM YYYY",
+                yValueFormatString: "$#,##0.#",
+                dataPoints: [
+                    { x: new Date(2016, 0, 1),  y: 19034.5 },
+                    { x: new Date(2016, 1, 1), y: 20015 },
+                    { x: new Date(2016, 2, 1), y: 27342 },
+                    { x: new Date(2016, 3, 1),  y: 20088 },
+                    { x: new Date(2016, 4, 1),  y: 20234 },
+                    { x: new Date(2016, 5, 1),  y: 29034 },
+                    { x: new Date(2016, 6, 1), y: 30487 },
+                    { x: new Date(2016, 7, 1), y: 32523 },
+                    { x: new Date(2016, 8, 1),  y: 20234 },
+                    { x: new Date(2016, 9, 1),  y: 27234 },
+                    { x: new Date(2016, 10, 1),  y: 33548 },
+                    { x: new Date(2016, 11, 1), y: 32534 }
+                ]
+            },
+        ]
+        };
+        $("#liveDiv").CanvasJSChart(options);
+        }
+
+        function toggleDataSeries(e) {
+            if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+            } else {
+                e.dataSeries.visible = true;
+            }
+            e.chart.render();
+        }
         
+        
+
 });
-// 
-// 
-// 
-// link to live reports
-// https://canvasjs.com/jquery-charts/chart-with-multiple-axes/
-// window.onload = function () {
-// const options = {
-// 	exportEnabled: true,
-// 	animationEnabled: true,
-// 	title:{
-// 		text: "Your selected cryptocurrencies rate in USD"
-// 	},
-// 	subtitles: [{
-// 		text: "Click Legend to Hide or Unhide Data Series"
-// 	}],
-// 	axisX: {
-// 		title: "States"
-// 	},
-// 	axisY: {
-// 		title: "Coin Value",
-// 		titleFontColor: "#4F81BC",
-// 		lineColor: "#4F81BC",
-// 		labelFontColor: "#4F81BC",
-// 		tickColor: "#4F81BC"
-// 	},
-// 	axisY2: {
-// 		title: "Profit in USD",
-// 		titleFontColor: "#C0504E",
-// 		lineColor: "#C0504E",
-// 		labelFontColor: "#C0504E",
-// 		tickColor: "#C0504E"
-// 	},
-// 	toolTip: {
-// 		shared: true
-// 	},
-// 	legend: {
-// 		cursor: "pointer",
-// 		itemclick: toggleDataSeries
-// 	},
-// 	data: [{
-// 		type: "spline",
-// 		name: "Units Sold",
-// 		showInLegend: true,
-// 		xValueFormatString: "MMM YYYY",
-// 		yValueFormatString: "#,##0 Units",
-// 		dataPoints: [
-// 			{ x: new Date(2016, 0, 1),  y: 120 },
-// 			{ x: new Date(2016, 1, 1), y: 135 },
-// 			{ x: new Date(2016, 2, 1), y: 144 },
-// 			{ x: new Date(2016, 3, 1),  y: 103 },
-// 			{ x: new Date(2016, 4, 1),  y: 93 },
-// 			{ x: new Date(2016, 5, 1),  y: 129 },
-// 			{ x: new Date(2016, 6, 1), y: 143 },
-// 			{ x: new Date(2016, 7, 1), y: 156 },
-// 			{ x: new Date(2016, 8, 1),  y: 122 },
-// 			{ x: new Date(2016, 9, 1),  y: 106 },
-// 			{ x: new Date(2016, 10, 1),  y: 137 },
-// 			{ x: new Date(2016, 11, 1), y: 142 }
-// 		]
-// 	},
-// 	{
-// 		type: "spline",
-// 		name: "Profit",
-// 		axisYType: "secondary",
-// 		showInLegend: true,
-// 		xValueFormatString: "MMM YYYY",
-// 		yValueFormatString: "$#,##0.#",
-// 		dataPoints: [
-// 			{ x: new Date(2016, 0, 1),  y: 19034.5 },
-// 			{ x: new Date(2016, 1, 1), y: 20015 },
-// 			{ x: new Date(2016, 2, 1), y: 27342 },
-// 			{ x: new Date(2016, 3, 1),  y: 20088 },
-// 			{ x: new Date(2016, 4, 1),  y: 20234 },
-// 			{ x: new Date(2016, 5, 1),  y: 29034 },
-// 			{ x: new Date(2016, 6, 1), y: 30487 },
-// 			{ x: new Date(2016, 7, 1), y: 32523 },
-// 			{ x: new Date(2016, 8, 1),  y: 20234 },
-// 			{ x: new Date(2016, 9, 1),  y: 27234 },
-// 			{ x: new Date(2016, 10, 1),  y: 33548 },
-// 			{ x: new Date(2016, 11, 1), y: 32534 }
-// 		]
-// 	}]
-// };
-// $("#liveDiv").CanvasJSChart(options);
-// function toggleDataSeries(e) {
-// 	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-// 		e.dataSeries.visible = false;
-// 	} else {
-// 		e.dataSeries.visible = true;
-// 	}
-// 	e.chart.render();
-// }
-// }
