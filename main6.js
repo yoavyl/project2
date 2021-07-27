8620673e8acfa3231a87f888f114a58e50da1deb
yoavyl <yoav.lip@gmail.com>
2021-07-27 08:50:17 +0300

    small chart cleaning

--- main6.js
+++ main6.js
@@ -298,38 +298,6 @@ $(function () {
             }
         }
 
-        // switch (topArr.length) {
-        //     case 1: visibilityGraph = [{visibility: true, showInLegend: true},
-        //      {visibility: false, showInLegend: false}, {visibility: false, showInLegend: false}, {visibility: false, showInLegend: false},
-        //     {visibility:false, showInLegend: false}]; 
-        //     for (i=1; i<=4; i++) {
-        //         topArrMap.push(x);
-        //     }; break;
-        //     case 2: visibilityGraph = [{visibility: true, showInLegend: true}, 
-            // {visibility: true, showInLegend: true},
-            // {visibility :false, showInLegend: false}, 
-            //     { visibility: false, showInLegend: false}, { visibility: false, tshowInLegend: false}];
-            // for (i=1; i<=3; i++) {
-            //     topArrMap.push(x);
-            // }; break;
-            // case 3: visibilityGraph = [{visibility: true, showInLegend: true}, 
-            // {visibility: true, showInLegend: true},
-            //  {visibility: true, showInLegend: true}, 
-            //     {visibility: false, showInLegend: false}, {visibility: false, showInLegend: false}];
-            //     topArrMap.push(x); topArrMap.push(x); break;
-            // case 4: visibilityGraph = [{visibility: true, showInLegend: true}, 
-            // {visibility: true, showInLegend: true}, 
-        //     {visibility: true, showInLegend: true}, 
-        //         {visibility: true, showInLegend: true}, 
-        //         {visibility: false, showInLegend: false}]; topArrMap.push(x); break;
-        //     case 5: visibilityGraph = [{visibility: true, showInLegend: true}, 
-        //     {visibility: true, showInLegend: true},
-        //     {visibility: true, showInLegend: true},
-        //      {visibility: true, showInLegend: true},
-        //      {visibility: true, showInLegend: true}];
-
-        // }
-
         var dataPoints1 = [];
         var dataPoints2 = [];
         var dataPoints3 = [];
@@ -348,7 +316,7 @@ $(function () {
                 suffix: "USD"
             },
             toolTip: {
-                shared: false
+                shared: false // affects the index at the bottom
                 // format with content formatter https://canvasjs.com/docs/charts/chart-options/tooltip/content-formatter/
             },
             legend: {
@@ -499,90 +467,4 @@ $(function () {
         setInterval(function () { updateChart() }, updateInterval);
         
         }
-        // var options = {
-        //     width: 800,
-        //     exportEnabled: true,
-        //     animationEnabled: true,
-        //     title:{
-        //         text: "Your selected cryptocurrencies rate in USD"
-        //     },
-        //     subtitles: [{
-        //         text: "Click Legend to Hide or Unhide Data Series"
-        //     }],
-        //     axisX: {
-        //         title: "seconds"
-        //     },
-        //     axisY: {
-        //         title: "USD",
-        //         titleFontColor: "#4F81BC",
-        //         lineColor: "#4F81BC",
-        //         labelFontColor: "#4F81BC",
-        //         tickColor: "#4F81BC"
-        //     },
-        //     toolTip: {
-        //         shared: true
-        //     },
-        //     legend: {
-        //         cursor: "pointer",
-        //         itemclick: toggleDataSeries
-        //     },
-        //     data: [{
-        //         type: "spline",
-        //         name: `${topArr[0].symbol}`,
-        //         showInLegend: true,
-        //         xValueFormatString: "MMM YYYY",
-        //         yValueFormatString: "#,##0 Units",
-        //         dataPoints: [
-        //             { x: new Date(2016, 0, 1),  y: 120 },
-        //             { x: new Date(2016, 1, 1), y: 135 },
-        //             { x: new Date(2016, 2, 1), y: 144 },
-        //             { x: new Date(2016, 3, 1),  y: 103 },
-        //             { x: new Date(2016, 4, 1),  y: 93 },
-        //             { x: new Date(2016, 5, 1),  y: 129 },
-        //             { x: new Date(2016, 6, 1), y: 143 },
-        //             { x: new Date(2016, 7, 1), y: 156 },
-        //             { x: new Date(2016, 8, 1),  y: 122 },
-        //             { x: new Date(2016, 9, 1),  y: 106 },
-        //             { x: new Date(2016, 10, 1),  y: 137 },
-        //             { x: new Date(2016, 11, 1), y: 142 }
-        //         ]
-        //     },
-        //     {
-        //         type: "spline",
-        //         name: `${topArr[1].symbol}`,
-        //         axisYType: "secondary",
-        //         showInLegend: true,
-        //         xValueFormatString: "MMM YYYY",
-        //         yValueFormatString: "$#,##0.#",
-        //         dataPoints: [
-        //             { x: new Date(2016, 0, 1),  y: 19034.5 },
-        //             { x: new Date(2016, 1, 1), y: 20015 },
-        //             { x: new Date(2016, 2, 1), y: 27342 },
-        //             { x: new Date(2016, 3, 1),  y: 20088 },
-        //             { x: new Date(2016, 4, 1),  y: 20234 },
-        //             { x: new Date(2016, 5, 1),  y: 29034 },
-        //             { x: new Date(2016, 6, 1), y: 30487 },
-        //             { x: new Date(2016, 7, 1), y: 32523 },
-        //             { x: new Date(2016, 8, 1),  y: 20234 },
-        //             { x: new Date(2016, 9, 1),  y: 27234 },
-        //             { x: new Date(2016, 10, 1),  y: 33548 },
-        //             { x: new Date(2016, 11, 1), y: 32534 }
-        //         ]
-        //     },
-        // ]
-        // };
-        // $("#liveDiv").CanvasJSChart(options);
-        // }
-
-        // function toggleDataSeries(e) {
-        //     if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-        //         e.dataSeries.visible = false;
-        //     } else {
-        //         e.dataSeries.visible = true;
-        //     }
-        //     e.chart.render();
-        // }
-        
-        
-
 });
